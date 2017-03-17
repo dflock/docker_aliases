2714aa2ab8b7c73a0dff75e3350b7dd17c48b524
Duncan Lock <dlock@phemi.com>
2017-03-17 15:48:12 -0700

    Better support for systemd/upstart in docker_wipe
    - Added init_type function
    - Added docker_ctl function to stop/start/restart docker. Handles
      different init systems (systemd/upstart) internally
    - Updated docker_wipe to remove some more /var/lib/docker folders,
      and to use docker_ctl to stop/start docker.

--- docker_aliases.sh
+++ docker_aliases.sh
@@ -145,6 +145,67 @@ docker_links() {
 alias dlinks='docker_links'
 
 #
+# Returns the systems init type as a string:
+#
+# 'upstart', 'systemd', 'sysv'
+#
+# Call like this:
+#
+# VAR_INIT_TYPE=$(init_type)
+#
+function init_type() {
+  if [[ $(/sbin/init --version &>/dev/null) =~ upstart ]]; then
+    echo 'upstart';
+  elif [[ $(systemctl) =~ -\.mount ]]; then
+    echo 'systemd';
+  elif [[ -f /etc/init.d/cron && ! -h /etc/init.d/cron ]]; then
+    echo 'sysv';
+  else
+    echo '';
+  fi
+}
+
+#
+# Stops, starts or restarts the docker daemon.
+# Should work on both upstart, systemd init systems.
+#
+# @1  The action to perform: start|stop|restart
+#
+docker_ctl() {
+  local action="$1"
+  local init=$(init_type)
+
+  case $init in
+    upstart)
+      case $action in
+        start )
+          sudo start docker
+          ;;
+        stop )
+          sudo stop docker
+          ;;
+        restart )
+          sudo restart docker
+          ;;
+      esac
+    ;;
+    systemd)
+      case $action in
+        start )
+          sudo systemctl start docker.service
+          ;;
+        stop )
+          sudo systemctl stop docker.service
+          ;;
+        restart )
+          sudo systemctl restart docker.service
+          ;;
+      esac
+    ;;
+  esac
+}
+
+#
 #  Delete all containers & images,
 #  reset dockers container linking DB and restart docker.
 #  The nuclear option.
@@ -155,14 +216,14 @@ docker_wipe() {
   $DSUDO docker rm -f $(docker ps -a -q)
   $DSUDO docker rmi -f $(docker images -q)
 
-  sudo systemctl stop docker.service || true
-  sudo stop docker || true
+  docker_ctl 'stop'
 
   sudo rm -f /var/lib/docker/linkgraph.db
   sudo rm -rf /var/lib/docker/aufs/diff/*
+  sudo rm -rf /var/lib/docker/network/*
+  sudo rm -rf /var/lib/docker/containers/*
 
-  sudo systemctl start docker.service || true
-  sudo start docker || true
+  docker_ctl 'start'
 }
 
 #
